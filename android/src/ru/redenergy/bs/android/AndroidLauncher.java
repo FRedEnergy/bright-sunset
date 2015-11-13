e5448cca52c08c1485efcc17797a6ed0e24eac2e
unknown <forwot163@gmail.com>
2015-11-13 17:15:44 +0400

    Initialized empty project

---
+++ android/src/ru/redenergy/bs/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.redenergy.bs.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.redenergy.bs.BrightSunsetGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BrightSunsetGame(), config);
+	}
+}
