c29a17a87e5c6e4e2084e2c7c758ea378da44c61
FRedEnergy <forwot163@gmail.com>
2016-03-01 16:57:00 +0300

    Implemented simple main menu

---
+++ core/src/ru/redenergy/bs/screen/MainMenuScreen.java
@@ -0,0 +1,79 @@
+package ru.redenergy.bs.screen;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import ru.redenergy.bs.BrightSunsetGame;
+
+public class MainMenuScreen implements Screen {
+
+    private Stage stage;
+
+    @Override
+    public void show() {
+        stage = new Stage();
+
+        Table mainTable = new Table();
+
+        mainTable.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        mainTable.center();
+        mainTable.add(new Label("Hello World", new Label.LabelStyle(new BitmapFont(), Color.WHITE)));
+        mainTable.row();
+        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+        style.font = new BitmapFont();
+        TextButton button = new TextButton("Game", style);
+        button.addListener(new ClickListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                BrightSunsetGame.instance.setScreen(new GameScreen());
+                return true;
+            }
+        });
+        mainTable.add(button);
+
+        stage.addActor(mainTable);
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
