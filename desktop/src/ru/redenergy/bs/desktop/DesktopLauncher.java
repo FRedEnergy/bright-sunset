f5a71950380df5c1821aeba7fdcffd1a74d630cc
RedEnergy <forwot163@gmail.com>
2016-03-03 21:59:26 +0400

    Increased desktop resolution and window now is non resizeable

--- desktop/src/ru/redenergy/bs/desktop/DesktopLauncher.java
+++ desktop/src/ru/redenergy/bs/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import ru.redenergy.bs.BrightSunsetGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.resizable = false;
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new BrightSunsetGame(), config);
 	}
 }
