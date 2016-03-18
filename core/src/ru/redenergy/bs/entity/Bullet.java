23be51cf31ecbf4e863c9972012be80ef9ec5f9e
FRedEnergy <forwot163@gmail.com>
2016-03-18 17:00:17 +0300

    Implemented Bullet entity

---
+++ core/src/ru/redenergy/bs/entity/Bullet.java
@@ -0,0 +1,33 @@
+package ru.redenergy.bs.entity;
+
+
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Bullet extends Entity {
+    public Bullet(World world, float x, float y) {
+        super(world, x, y);
+    }
+
+    @Override
+    protected void initBodyAtCords(World world, float x, float y) {
+        this.world = world;
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.KinematicBody;
+        bodyDef.position.set(x, y);
+        bodyDef.bullet = true;
+        body = world.createBody(bodyDef);
+
+        CircleShape shape = new CircleShape();
+        shape.setRadius(0.5F);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+
+        body.createFixture(fixtureDef);
+        shape.dispose();
+    }
+}
