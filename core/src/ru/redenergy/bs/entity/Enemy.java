f62409a531cc6eba49880e6deea276c3b88d3da1
RedEnergy <forwot163@gmail.com>
2016-04-15 00:25:15 +0400

    Enemy now has zombie texture

--- core/src/ru/redenergy/bs/entity/Enemy.java
+++ core/src/ru/redenergy/bs/entity/Enemy.java
@@ -1,12 +1,25 @@
 package ru.redenergy.bs.entity;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.QueryCallback;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 
+import java.util.concurrent.TimeUnit;
+
 public class Enemy extends EntityLiving {
 
+    protected static Sprite charTexture = new Sprite(new Texture(Gdx.files.internal("zombie1.png")));
+    private static final float damage = 2.75F;
+    private long lastAttackTime = 0L;
+    private static final long attackDelay = TimeUnit.SECONDS.toMillis(2);
 
     public Enemy(World world, float x, float y) {
         super(world, x, y);
@@ -40,5 +53,38 @@ public class Enemy extends EntityLiving {
         float impulseX = body.getMass() * changeX / (1F / 60F);
         float impulseY = body.getMass() * changeY / (1F / 60F);
         body.applyForce(new Vector2(impulseX, impulseY), body.getWorldCenter(), true);
+
+        world.QueryAABB(new QueryCallback() {
+            @Override
+            public boolean reportFixture(Fixture fixture) {
+                if(fixture.getBody().getUserData() instanceof Player){
+                    Player pl = (Player) fixture.getBody().getUserData();
+                    long current = System.currentTimeMillis();
+                    if((current - lastAttackTime) > attackDelay) {
+                        pl.setHealth(pl.getHealth() - damage);
+                        lastAttackTime = current;
+                    }
+                    return true;
+                }
+                return false;
+            }
+        }, body.getPosition().x - 5, body.getPosition().y - 5, body.getPosition().x + 5, body.getPosition().y + 5);
+    }
+
+    @Override
+    public void render(SpriteBatch batch, OrthographicCamera camera) {
+        super.render(batch, camera);
+        charTexture.setScale(0.05F);
+        charTexture.setPosition(body.getPosition().x - charTexture.getWidth() / 2, body.getPosition().y - charTexture.getHeight() / 2);
+        charTexture.setRotation((float) Math.toDegrees(this.body.getAngle()));
+        charTexture.draw(batch);
+    }
+
+    public long getLastAttackTime() {
+        return lastAttackTime;
+    }
+
+    public void setLastAttackTime(long lastAttackTime) {
+        this.lastAttackTime = lastAttackTime;
     }
 }
